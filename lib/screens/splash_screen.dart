befe7b7d29e56334f855424598ce1180fb1753f1
M-J-Christina213 <m.j.christina213@gmail.com>
2025-07-12 15:18:08 +0530

    Added splash screen in kuweni 💗

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.pets, size: 80, color: Colors.orange),
+            SizedBox(height: 20),
+            Text("Welcome to Kuveni App",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
