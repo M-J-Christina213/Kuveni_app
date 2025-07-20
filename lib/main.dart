ef64a852dcdc493c92ce018a966ee48df5197c2e
M-J-Christina213 <m.j.christina213@gmail.com>
2025-07-20 23:51:35 +0530

    import issue 😊

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:kuveni_app/screens/bottom_nav_bar.dart';
-import 'package:kuveni_app/screens/logout_screen';
+import 'package:kuveni_app/screens/logout_screen.dart';
 import 'firebase_options.dart';
 
 // Core Screens
