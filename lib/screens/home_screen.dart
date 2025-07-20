58077562ea02534649fbf7d9ee1ab896423922df
M-J-Christina213 <m.j.christina213@gmail.com>
2025-07-20 22:25:52 +0530

    finally solved the git issues after 1 hour 🥲

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,14 +3,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kuveni_app/screens/bottom_nav_bar.dart';
-import 'package:kuveni_app/screens/bottom_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
   
-  
   @override
-  _HomeScreenState createState() => _HomeScreenState();
   _HomeScreenState createState() => _HomeScreenState();
 }
 
